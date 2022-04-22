7ba4949b9abc7ad9453ebcc03673afd836c8c99b
Junnyjun <chbe5082@naver.com>
2022-04-22 23:46:50 +0900

    :fire: chore (Main) : 필요없는 부분 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,10 @@
-import core.Entity;
 import entity.Member;
-import jdbc.JdbcConnector;
-import jdbc.Reader;
 import lib.JpaImpl;
 
-import java.beans.IntrospectionException;
-import java.io.FileNotFoundException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IntrospectionException, SQLException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchFieldException, FileNotFoundException {
-//        Reader reader = new Reader();
-//        reader.getYaml();
-
-        JdbcConnector jdbcConnector = new JdbcConnector();
+    public static void main(String[] args) {
         List<Member> data  = JpaImpl.getInstance().findAll();
-
-
-        idFieldValidation("entity.Member/**/");
-
-//        MemberRepositoryImpl.getInstance().findAll();
-    }
-
-    private static void idFieldValidation(String classNameWithPackage) throws ClassNotFoundException {
-        Class c = Class.forName(classNameWithPackage);
-        String simpleName = c.getSimpleName();
-        Method[] declaredMethods = c.getDeclaredMethods();
-        Class[] declaredClasses = c.getDeclaredClasses();
-        Method[] methods = c.getMethods();
-        Class<? extends Class> aClass = c.getClass();
-
-        Entity entity = (Entity) c.getAnnotation(Entity.class);
-
-        Arrays.stream(declaredMethods).forEach(e ->{
-            System.out.println("e = " + e);
-            Entity annotation = e.getAnnotation(Entity.class);
-        });
-
     }
 }
