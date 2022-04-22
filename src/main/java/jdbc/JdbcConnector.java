c9080d01810fc716e1cbda7b4fb9d01fdb9be01c
Junnyjun <chbe5082@naver.com>
2022-04-22 23:40:23 +0900

    :fire: refactor(JdbcConnector) : DB Connector 부분 의존성 변경

--- src/main/java/jdbc/JdbcConnector.java
+++ src/main/java/jdbc/JdbcConnector.java
@@ -1,37 +1,27 @@
 package jdbc;
 
-import java.io.FileNotFoundException;
-import java.sql.*;
-import java.util.List;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class JdbcConnector {
+    private final DataSource dataSource;
 
-    private Connection conn  = null;
+    public JdbcConnector() throws SQLException {
+        this.dataSource = new MyDataSource();
+    }
 
-    public JdbcConnector() throws ClassNotFoundException, FileNotFoundException, SQLException {
-        try{
-            DataSourceValue value = Reader.getYaml();
-           this.conn = DriverManager.getConnection(value.getUrl(),value.getId(),value.getPassword());
+    public ResultSet queryProvider(String query){
+        try( Connection connection = dataSource.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query);
+             ResultSet resultSet = pstmt.executeQuery();
+        ){
+            return resultSet;
         }catch (SQLException e){
-            throw new SQLException("DATASOURCE WRONG");
+            throw new IllegalArgumentException("SQL IS WRONG");
         }
-    }
-    public void checkConnection() throws SQLException {
-        Statement stmt = conn.createStatement();
-        PreparedStatement pstmt = null;
-        pstmt = conn.prepareStatement("select 1");
-        ResultSet rs = pstmt.executeQuery();
-
-        while(rs.next()) {
-            String result = rs.getString("1");
-            System.out.println("result allways 1 : "+ result);
-        }
-    }
-
-    public ResultSet selectAll(String query) throws SQLException {
-        PreparedStatement pstmt = null;
-        pstmt = conn.prepareStatement(query);
-        return pstmt.executeQuery();
     }
 
 }
